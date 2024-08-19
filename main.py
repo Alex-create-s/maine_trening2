e9b8953bb0c5596085c505a2dff0274d9865a3e5
Alex-create-s <alexanderpu6@mail.ru>
2024-08-19 17:53:28 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+name = "курс_Python"
+new_name ="всего_задач"
+задач_count ="12"
+first_name ="затрачено_часов"
+часов_count ="1,5"
+second_name ="среднее время выполнения"
+время_count ="0,125"
+print(name,new_name,задач_count,first_name,часов_count,second_name,время_count)
