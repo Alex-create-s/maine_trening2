96bd24a8dd55d15e48728ea1dec251b780e6542c
Alex-create-s <alexanderpu6@mail.ru>
2024-08-20 11:13:37 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -4,5 +4,5 @@ zadach_count =12.0
 first_name ="zatracheno_chasov"
 chasov_count =1.5
 second_name ="srednee_vremia_vypolneniia"
-vremia_count =(1.5/12.0)
+vremia_count =(chasov_count/zadach_count)
 print(name,new_name,zadach_count,first_name,chasov_count,second_name,vremia_count)
